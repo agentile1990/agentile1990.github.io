6629520960a7bca8e4d09db3a6eb025d05842186
Andrew Gentile <agentile1990@gmail.com>
2020-08-01 11:20:24 -0500

    Add Courses Page

---
+++ src/views/Courses.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="full-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="9" md="8" lg="7"> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Courses",
+  data: () => ({}),
+  methods: {}
+};
+</script>
